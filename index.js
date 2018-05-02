dced711f726ec0b6bec007456317e204cfd78e1f
Sebastian Restrepo Quiceno <1144103496@academico.icesi.edu.co>
2018-05-02 08:16:31 -0500

    Hello

---
+++ index.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient,
+    express = require('express'),
+    consolidate = require('consolidate');
+
+var app = express(),
+    db;
+
+app.engine('hbs', consolidate.handlebars);
+app.set('views', './views');
+app.set('views engine', 'hbs');
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) throw err;
+
+    db = client.db('facebook');
+
+    app.listen(1234);
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
